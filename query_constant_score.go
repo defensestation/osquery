539fb219534c4de1d4e837900ef0917e4cc8cef2
Sushmita <s.sushmita@zomato.com>
2025-02-11 20:16:47 +0530

    added named query support in constant score query (#4)

--- query_constant_score.go
+++ query_constant_score.go
@@ -12,6 +12,7 @@ import "github.com/fatih/structs"
 type ConstantScoreQuery struct {
 	filter Mappable
 	boost  float32
+	name   string
 }
 
 // ConstantScore creates a new query of type "constant_score" with the provided
@@ -28,6 +29,13 @@ func (q *ConstantScoreQuery) Boost(b float32) *ConstantScoreQuery {
 	return q
 }
 
+// Name sets the name of the query that is returned in matched_queries in response
+// if document matches the query.
+func (q *ConstantScoreQuery) Name(name string) *ConstantScoreQuery {
+	q.name = name
+	return q
+}
+
 // Map returns a map representation of the query, thus implementing the
 // Mappable interface.
 func (q *ConstantScoreQuery) Map() map[string]interface{} {
@@ -35,6 +43,7 @@ func (q *ConstantScoreQuery) Map() map[string]interface{} {
 		"constant_score": structs.Map(struct {
 			Filter map[string]interface{} `structs:"filter"`
 			Boost  float32                `structs:"boost,omitempty"`
-		}{q.filter.Map(), q.boost}),
+			Name   string                 `structs:"_name,omitempty"`
+		}{q.filter.Map(), q.boost, q.name}),
 	}
 }
